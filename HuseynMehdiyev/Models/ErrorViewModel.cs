944df219f8baf212dced438e6cc99da430d5c29f
mhuseynn <hmehdiyev2005@gmail.com>
2025-01-08 21:48:00 +0400

    Add project files.

---
+++ HuseynMehdiyev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HuseynMehdiyev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
